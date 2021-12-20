9746af4083f405646fd2990512b57e73e8aeeb03
Matej Glejtek <matejglejtek@gmail.com>
2021-12-20 15:45:38 +0100

    add context param to wifi scanner; register listener when wifi scan finishes

--- android/src/main/kotlin/cz/dronetag/flutter_opendroneid/scanner/WifiScanner.kt
+++ android/src/main/kotlin/cz/dronetag/flutter_opendroneid/scanner/WifiScanner.kt
@@ -1,6 +1,7 @@
 package cz.dronetag.flutter_opendroneid
 
 import android.net.wifi.WifiManager
+import android.net.wifi.ScanResult
 import android.os.Build
 import android.os.ParcelUuid
 import android.content.*
@@ -10,10 +11,11 @@ import cz.dronetag.flutter_opendroneid.models.OdidMessage
 import java.util.*
 import io.flutter.Log
 
-class WifiScanner(
+class WifiScanner (
     private val messagesHandler: StreamHandler,
     private val bluetoothStateHandler: StreamHandler,
-    private val wifiManager: WifiManager?
+    private val wifiManager: WifiManager?,
+    private val context: Context
 ) {
 
     private val serviceUuid = UUID.fromString("0000fffa-0000-1000-8000-00805f9b34fb")
@@ -21,24 +23,23 @@ class WifiScanner(
     private val odidAdCode = byteArrayOf(0x0D.toByte())
 
     private val messageHandler = OdidMessageHandler()
-    private val resultList = ArrayList<ScanResult>()
+    val resultList = ArrayList<ScanResult>()
 
     private val broadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(contxt: Context?, intent: Intent?) {
-            resultList = wifiManager.scanResults as ArrayList<ScanResult>
+            val resultList = wifiManager?.scanResults as ArrayList<ScanResult>
             Log.d("TESTING", "onReceive Called")
         }
     }
 
     fun cancel() {
-        wifiManager?.startScan()
-        Log.d("wifi scanner", "startScan begin")
+        Log.d("wifi scanner", "startScan cancel")
     }
 
     fun scan() {
         wifiManager?.startScan()
 
-        registerReceiver(broadcastReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+        context.registerReceiver(broadcastReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
         Log.d("wifi scanner", "startScan begin")
     }
 }
