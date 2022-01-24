79541f6932907fc25db32d8aef5d9e45584637c6
Matej Glejtek <matejglejtek@gmail.com>
2022-01-24 16:54:48 +0100

    remove CoreWlan package import from wifi scanner

--- ios/Classes/Scanner/WifiScanner.swift
+++ ios/Classes/Scanner/WifiScanner.swift
@@ -1,47 +1,25 @@
 import Foundation
-import CoreWLAN
 
 class WifiScanner: NSObject{
     private let messageHandler: StreamHandler
     private let stateHandler: StreamHandler
     
-    var wifiClient: CWWifiClient
-    let dispatchQueue: DispatchQueue = DispatchQueue(label: "BluetoothScanner")
+    let dispatchQueue: DispatchQueue = DispatchQueue(label: "WifiScanner")
     
-    static let serviceUUID = CBUUID(string: "0000fffa-0000-1000-8000-00805f9b34fb")
     
     init(messageHandler: StreamHandler, stateHandler: StreamHandler) {
         self.messageHandler = messageHandler
         self.stateHandler = stateHandler
-        self.wifiClient = CWWifiClient.shared()
         super.init()
-        self.centralManager.delegate = self
     }
     
     func scan() {
-        /*if centralManager.isScanning == true { return }
-        
-        guard centralManager.state == .poweredOn else {
-            NSLog("Couldn't start BLE scan, because central is not powered on")
-            return
-        }
-        
-        centralManager.scanForPeripherals(
-            withServices: nil,
-            options: [
-                CBCentralManagerScanOptionAllowDuplicatesKey: true,
-            ]
-        )*/
+        // to-do
     }
     
     func cancel() {
-        //centralManager.stopScan()
+        // to-do
     }
     
-    
-    private func getOdidPayload(from advertisementData: [String : Any]) -> Data? {
-        // Peripheral must have service data
-       
-    }
     
 }
