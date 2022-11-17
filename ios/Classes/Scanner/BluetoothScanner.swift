875287bdb76c79d0d2f15d9cbba6f51dde049668
Matej Glejtek <matejglejtek@gmail.com>
2022-11-17 21:07:40 +0100

    feat: Add support for Bluetooth message packs (#7)

--- ios/Classes/Scanner/BluetoothScanner.swift
+++ ios/Classes/Scanner/BluetoothScanner.swift
@@ -70,6 +70,10 @@ class BluetoothScanner: NSObject, CBCentralManagerDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        handleOdidMessage(advertisementData: advertisementData, didDiscover: peripheral, rssi: RSSI, offset: 6)
+    }
+
+    private func handleOdidMessage(advertisementData: [String : Any], didDiscover peripheral: CBPeripheral, rssi RSSI: NSNumber, offset: NSNumber){
         guard let data = getOdidPayload(from: advertisementData) else {
             // This advertisement is not an ODID ad data
             return
@@ -77,43 +81,56 @@ class BluetoothScanner: NSObject, CBCentralManagerDelegate {
         
         do {
             var err: FlutterError?
-            let typeOrdinal = UInt(exactly: dataParser.determineMessageTypePayload(data, offset: 6, error: &err)!)
+            let typeOrdinal = UInt(exactly: dataParser.determineMessageTypePayload(data, offset: offset, error: &err)!)
             let type = DTGMessageType(rawValue: typeOrdinal!)
             if(type == DTGMessageType.basicId)
             {
-                let message : DTGBasicIdMessage? = dataParser.fromBufferBasicPayload(data, offset: 6, macAddress: peripheral.identifier.uuidString, error: &err)
+                let message : DTGBasicIdMessage? = dataParser.fromBufferBasicPayload(data, offset: offset, macAddress: peripheral.identifier.uuidString, error: &err)
                 message!.rssi = RSSI.intValue as NSNumber
                 basicMessageHandler.send(message!.toMap() as Any)
             }
             else if(type == DTGMessageType.location)
             {
-                let message : DTGLocationMessage? = dataParser.fromBufferLocationPayload(data, offset: 6, macAddress: peripheral.identifier.uuidString, error: &err)
+                let message : DTGLocationMessage? = dataParser.fromBufferLocationPayload(data, offset: offset, macAddress: peripheral.identifier.uuidString, error: &err)
                 message!.rssi = RSSI.intValue as NSNumber
                 locationMessageHandler.send(message!.toMap() as Any)
             }
             else if(type == DTGMessageType.operatorId)
             {
-                let message : DTGOperatorIdMessage? = dataParser.fromBufferOperatorIdPayload(data, offset: 6, macAddress: peripheral.identifier.uuidString, error: &err)
+                let message : DTGOperatorIdMessage? = dataParser.fromBufferOperatorIdPayload(data, offset: offset, macAddress: peripheral.identifier.uuidString, error: &err)
                 message!.rssi = RSSI.intValue as NSNumber
                 operatoridMessageHandler.send(message!.toMap() as Any)
             }
             else if(type == DTGMessageType.selfId)
             {
-                let message : DTGSelfIdMessage? = dataParser.fromBufferSelfIdPayload(data, offset: 6, macAddress: peripheral.identifier.uuidString, error: &err)
+                let message : DTGSelfIdMessage? = dataParser.fromBufferSelfIdPayload(data, offset: offset, macAddress: peripheral.identifier.uuidString, error: &err)
                 message!.rssi = RSSI.intValue as NSNumber
                 selfidMessageHandler.send(message!.toMap() as Any)
             }
             else if(type == DTGMessageType.auth)
             {
-                let message : DTGAuthenticationMessage? = dataParser.fromBufferAuthenticationPayload(data, offset: 6, macAddress: peripheral.identifier.uuidString, error: &err)
+                let message : DTGAuthenticationMessage? = dataParser.fromBufferAuthenticationPayload(data, offset: offset, macAddress: peripheral.identifier.uuidString, error: &err)
                 message!.rssi = RSSI.intValue as NSNumber
                 authMessageHandler.send(message!.toMap() as Any)
             }
             else if(type == DTGMessageType.system)
             {
-                let message : DTGSystemDataMessage? = dataParser.fromBufferSystemDataPayload(data, offset: 6, macAddress: peripheral.identifier.uuidString, error: &err)
+                let message : DTGSystemDataMessage? = dataParser.fromBufferSystemDataPayload(data, offset: offset, macAddress: peripheral.identifier.uuidString, error: &err)
                 message!.rssi = RSSI.intValue as NSNumber
                 systemMessageHandler.send(message!.toMap() as Any)
+            }
+            else if(type == DTGMessageType.messagePack)
+            {
+                let bytes = Array(data.data)
+                var packOffset = offset as! Int + 1
+                let messageSize = bytes[packOffset];
+                packOffset += 1
+                let messages = bytes[packOffset];
+                packOffset += 1
+                for _ in 0 ... (messages - 1) {
+                    handleOdidMessage(advertisementData: advertisementData, didDiscover: peripheral, rssi: RSSI, offset: packOffset as NSNumber)
+                    packOffset += Int(messageSize)
+                }
             }
         } catch {
             return
